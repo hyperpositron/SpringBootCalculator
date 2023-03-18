271fc83c2afc5a6580a2271473a348705b0f37ca
Александр <hyperpositron@gmail.com>
2023-03-18 19:38:55 +0200

    Подкапотка В CalculatorService.java sum

--- src/main/java/pro/sky/SpringBootCalculator/CalculatorService.java
+++ src/main/java/pro/sky/SpringBootCalculator/CalculatorService.java
@@ -1,4 +1,10 @@
 package pro.sky.SpringBootCalculator;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CalculatorService {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
 }
